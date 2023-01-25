fcb0e8aa66978979b179438aaab865773f48016f
sharmaviv11 <118325765+sharmaviv11@users.noreply.github.com>
2023-01-25 10:08:17 +0800

    Add files via upload

--- dbconnect.py
+++ dbconnect.py
@@ -1,6 +1,9 @@
 import pyodbc
 import pytest
-from numpy.core.defchararray import splitlines
+import pandas as pd
+import matplotlib.pyplot as plt
+import csv
+import sys
 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=OLP0TAQR02;'
@@ -24,19 +27,44 @@ conn = pyodbc.connect('Driver={SQL Server};'
 #     # cursor.close()
 #     # conn.close()
 #
+#
 # test_sql()
 
 
-def test_sql_list():
-    cursor = conn.cursor()
-    list = []
-    select_query = "select * from dbo.CONTRACT where CONTRACTCODE= "
-    ids = ["CONTRACTCODE", "DESCRIPTION", "COMPANYCODE"]
-    for x in ids:
-        var = select_query + x
-        cursor.execute(var)
-        list.extend(cursor.fetchall())
-    print(list[23])
+# def test_export_sql():
+#     sql_query = pd.read_sql_query('''
+#                                 select * from AcQuireODO.dbo.DRILLTYPE
+#                                 '''
+#                                   , conn)
+#     df = pd.DataFrame(sql_query)
+#     df.to_csv(r'C:\Users\sharvive\PycharmProjects\pythonProject\python\export_data.csv', index=False)
+#
+#
+# test_export_sql()
+
+def test_read_csv():
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    columns = ["DRILLTYPE", "DESCRIPTION"]
+    df = pd.read_csv(r'C:\Users\sharvive\PycharmProjects\pythonProject\python\export_data.csv', usecols=columns)
+    print("Contents in csv file:", df)
+    plt.plot(df.DRILLTYPE, df.DESCRIPTION)
+    # plt.plot(df.CONTRACTCODE, df.DESCRIPTION, df.COMPANYCODE, df.EXPIRYDATE, df.STARTDATE)
+    # plt.plot(df.astype({"CONTRACTCODE": int, "COMPANYCODE": int}))
+    plt.show()
 
 
-test_sql_list()
+test_read_csv()
+# def test_sql_list():
+#     cursor = conn.cursor()
+#     list = []
+#     select_query = "select * from dbo.CONTRACT where CONTRACTCODE= "
+#     ids = ["CONTRACTCODE", "DESCRIPTION", "COMPANYCODE"]
+#     for x in ids:
+#         var = select_query + x
+#         cursor.execute(var)
+#         list.extend(cursor.fetchall())
+#     print(list[23])
+#
+#
+# test_sql_list()
